daf20f5325b051f09cc5aa47a54d86f5036e94a0
Vansh Mishra <mishravansh831@gmail.com>
2025-12-15 14:31:11 +0530

    url changed

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // 🔑 IMPORTANT: credentials: true
 app.use(cors({
-  origin: "https://vercel.com/vansh-mishras-projects/orufy-assignment-zz19",
+  origin: "https://vercel.com/vansh-mishras-projects/orufy-assignment-zz19/",
   credentials: true
 }));
 
